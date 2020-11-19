30840c5baef79f31dc69fcb7c42ea87825ba45e2
kaneuffe <kai.neuffer@microsoft.com>
2020-11-19 19:22:36 +0100

    subnet_id changed

--- terraform/create_network.tf
+++ terraform/create_network.tf
@@ -101,8 +101,10 @@ resource "azurerm_subnet_network_security_group_association" "microhack_cc_subne
 }
 
 # Assign the netwrok security group to the Compute subnets
+for 
+
 resource "azurerm_subnet_network_security_group_association" "microhack_compute_subnet_nsg_assign" {
     count                     = var.nteams
-    subnet_id                 = azurerm_subnet.microhack_compute_subnet.*.id
+    subnet_id                 = azurerm_subnet.microhack_compute_subnet[*].id
     network_security_group_id = azurerm_network_security_group.microhack_compute_subnet_nsg.id
 }
