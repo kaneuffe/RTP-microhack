145f633b6dbdcd48f6632f9ecb0869189aafe721
kaneuffe <kai.neuffer@microsoft.com>
2020-11-19 17:32:06 +0100

    Fixed nsg errors

--- terraform/create_network.tf
+++ terraform/create_network.tf
@@ -75,8 +75,34 @@ resource "azurerm_network_security_group" "microhack_cc_subnet_nsg" {
     }
 }
 
-# Assign the netwrok security group to the subnet
+# Create a Network Security Group
+resource "azurerm_network_security_group" "microhack_compute_subnet_nsg" {
+    name                = "${var.prefix}-subnet-nsg"
+    location            = azurerm_resource_group.microhack_rg.location
+    resource_group_name = azurerm_resource_group.microhack_rg.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefixes    = list(var.cyclecloud_public_access_address_prefixes)
+        destination_address_prefix = "*"
+    }
+}
+
+# Assign the network security group to the CC subnet
 resource "azurerm_subnet_network_security_group_association" "microhack_cc_subnet_nsg_assign" {
-    subnet_id                 = azurerm_subnet.acc_subnet.id
-    network_security_group_id = azurerm_network_security_group.acc_subnet_nsg.id
+    subnet_id                 = azurerm_subnet.microhack_cc_subnet_subnet.*.id
+    network_security_group_id = azurerm_network_security_group.microhack_cc_subnet_nsg.id
+}
+
+# Assign the netwrok security group to the Compute subnets
+resource "azurerm_subnet_network_security_group_association" "microhack_cc_subnet_nsg_assign" {
+    count                     = var.nteams
+    subnet_id                 = azurerm_subnet.microhack_cc_subnet_subnet.*.id
+    network_security_group_id = azurerm_network_security_group.microhack_compute_subnet_nsg.id
 }
