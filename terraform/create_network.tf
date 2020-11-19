0ffe28e1fa540c0565a1fb7290c8556325c604bb
kaneuffe <kai.neuffer@microsoft.com>
2020-11-19 20:30:09 +0100

    Count 5

--- terraform/create_network.tf
+++ terraform/create_network.tf
@@ -30,7 +30,7 @@ resource "azurerm_subnet" "microhack_compute_subnet" {
   name                 = "${var.prefix}-compute-${count.index}-subnet"
   virtual_network_name = azurerm_virtual_network.microhack_vnet.name
   resource_group_name  = azurerm_resource_group.microhack_rg.name
-  address_prefixes     = ["10.${count.index}.0.0/24"]
+  address_prefixes     = ["10.${count.index + 1}.0.0/24"]
   service_endpoints    = ["Microsoft.Storage"]
 }
 
