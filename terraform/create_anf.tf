224f5ef685fc3adf77617b08be687d9aaba3ce1c
kaneuffe <kai.neuffer@microsoft.com>
2020-11-23 13:09:12 +0100

    zipmap

--- terraform/create_anf.tf
+++ terraform/create_anf.tf
@@ -41,6 +41,6 @@ data "azurerm_netapp_volume" "anf_volume" {
 }
 
 output "anf_mountpoints" {
-  value = data.azurerm_netapp_volume.anf_volume[*].mount_ip_addresses
+  value = zipmap(data.azurerm_netapp_volume.anf_volume[*].mount_ip_addresses, data.azurerm_netapp_volume.anf_volume[*].name)
   # value = zipmap(data.azurerm_netapp_volume.anf_volume.*.mount_ip_addresses, data.azurerm_netapp_volume.anf_volume.*.name)
 } 
