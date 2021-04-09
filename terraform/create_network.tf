41f8fd601172fdb245c3baef3a3f90a6a8cfeae5
kaneuffe <kai.neuffer@microsoft.com>
2021-04-09 20:57:25 +0200

    Add NSGprefix 2

--- terraform/create_network.tf
+++ terraform/create_network.tf
@@ -55,7 +55,7 @@ resource "azurerm_network_security_group" "microhack_cc_subnet_nsg" {
         protocol                   = "Tcp"
         source_port_range          = "*"
         destination_port_range     = "22"
-        source_address_prefixes    = var.cyclecloud_public_access_address_prefixes
+        source_address_prefixes    = list(var.cyclecloud_public_access_address_prefixes)
         destination_address_prefix = "*"
     }
     security_rule {
