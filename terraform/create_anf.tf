552b725202225e8a6aeeb8a736bced382a36ebb9
kaneuffe <kai.neuffer@microsoft.com>
2021-04-12 17:46:11 +0200

    Annded ANF export rule 5

--- terraform/create_anf.tf
+++ terraform/create_anf.tf
@@ -31,10 +31,9 @@ resource "azurerm_netapp_volume" "microhack_anf_volume" {
   depends_on          = [azurerm_netapp_pool.microhack_anf_pool]
 
   export_policy_rule {
-    rule_index      = 1
-    allowed_clients = [azurerm_subnet.microhack_cc_subnet.address_prefixes]
+    rule_index      = "${count.index + 1}"
+    allowed_clients = ["10.0.${count.index + 2}.0/24"]
   }
-
 }
 
 data "azurerm_netapp_volume" "anf_volume" {
