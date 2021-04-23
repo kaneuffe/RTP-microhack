f3e92f5326661cc3695a935f5e0e6f274d656a87
kaneuffe <kai.neuffer@microsoft.com>
2021-04-23 16:26:16 +0200

    Added scritps directory

---
+++ scripts/namd-sjob.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=namd
+#SBATCH --error=%x_%j.err
+#SBATCH --output=%x_%j.out
+#SBATCH --nodes=8
+#SBATCH --ntasks=960
+#SBATCH --partition=hpc
+
+#
+# Load modules
+#
+module load mpi/hpcx
+
+#
+# Switch to workdir
+#
+WORKDIR=/shared/home/kai/namd/namd-benchmarks/1400k-atoms/
+cd $WORKDIR
+#
+# Set INPUT variables
+#
+INPUT=benchmark.in
+
+#
+# Define variables
+#
+CHARMRUN="/sw/namd/charmrun +n ${SLURM_NTASKS} ++mpiexec ++remote-shell srun"
+SINGULARITY="`which singularity` exec --bind /opt,/etc/libibverbs.d,/usr/bin/srun,/var/run/munge,/usr/lib64/libmunge.so.2,/usr/lib64/libmunge.so.2.0.0,/run/munge,/etc/slurm,/sched,/usr/lib64/slurm /shared/home/team01/namd/namd-2.14.sif"
+NAMD2="/sw/namd/namd2"
+
+export SINGULARITYENV_PATH=${PATH}
+export SINGULARITYENV_LD_LIBRARY_PATH=${LD_LIBRARY_PATH}
+
+${SINGULARITY} ${CHARMRUN} ${SINGULARITY} ${NAMD2} ${INPUT}
