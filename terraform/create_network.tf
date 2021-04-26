37b1def6ecc399d1fd926ded04af29929896f5a2
kaneuffe <kai.neuffer@microsoft.com>
2021-04-26 21:37:10 +0200

    Chnaged NSG

--- terraform/create_network.tf
+++ terraform/create_network.tf
@@ -99,6 +99,28 @@ resource "azurerm_network_security_group" "microhack_compute_subnet_nsg" {
         source_address_prefixes    = [var.cyclecloud_public_access_address_prefix1, var.cyclecloud_public_access_address_prefix2]
         destination_address_prefix = "*"
     }
+    security_rule {
+        name                       = "HTTP"
+        priority                   = 1002
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefixes    = [var.cyclecloud_public_access_address_prefix1, var.cyclecloud_public_access_address_prefix2]
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name                       = "HTTPS"
+        priority                   = 1003
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "443"
+        source_address_prefixes    = [var.cyclecloud_public_access_address_prefix1, var.cyclecloud_public_access_address_prefix2]
+        destination_address_prefix = "*"
+    }
 }
 
 # Assign the network security group to the CC subnet
